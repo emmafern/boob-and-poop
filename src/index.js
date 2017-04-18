9ce5aee8dcf069d22f1d05eadf0b0d5cce4471ec
Emma Colner <emma@stitchfix.com>
2017-04-17 21:38:28 -0700

    working firebase email authentication, but no working react app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import firebase from 'firebase';
 import App from './App';
 import './index.css';
-
-const config = {
-  apiKey: "AIzaSyBIPkvrjHEtl5oNQnODhEi4u2KZtPAHYQo",
-  authDomain: "boob-and-poop.firebaseapp.com",
-  databaseURL: "https://boob-and-poop.firebaseio.com",
-  projectId: "boob-and-poop",
-};
-firebase.initializeApp(config)
 
 ReactDOM.render(
   <App />,
